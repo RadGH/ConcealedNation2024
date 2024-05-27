6a528a146026fd88f69b72c766c748a4e3263ecb
Radley <radley@alchemyandaim.com>
2024-05-27 13:17:08 -0700

    Ready for staging site review

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+(function() {
+	const o = this;
+
+	o.init = function() {
+	};
+
+	// On document ready, load init
+	document.addEventListener('DOMContentLoaded', o.init);
+
+	return o;
+})();
--- functions.php
+++ functions.php
@@ -8,12 +8,10 @@
  * @link    https://concealednation2024wp.com
  */
 
-namespace concealednation2024;
-
 /**
  * Set up theme defaults and register various WordPress features.
  */
-function setup() {
+function cn_setup() {
 
 	// Enqueue editor styles and fonts.
 	add_editor_style( 'style.css' );
@@ -21,31 +19,51 @@ function setup() {
 	// Remove core block patterns.
 	remove_theme_support( 'core-block-patterns' );
 }
-add_action( 'after_setup_theme', __NAMESPACE__ . '\setup' );
+add_action( 'after_setup_theme', 'cn_setup' );
 
 
 /**
  * Enqueue styles.
  */
-function enqueue_style_sheet() {
-	wp_enqueue_style( sanitize_title( __NAMESPACE__ ), get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+function cn_enqueue_theme_assets() {
+	// Check if host ends in .radgh.com, if so, load cache-busting version
+	if ( str_contains( $_SERVER['HTTP_HOST'], '.radgh.com' ) ) {
+		$v = max(
+			filemtime( get_template_directory() . '/style.css' ),
+			filemtime( get_template_directory() . '/assets/main.js' )
+		);
+	} else {
+		$v = wp_get_theme()->get( 'Version' );
+	}
+	
+	wp_enqueue_style( 'concealednation2024', get_template_directory_uri() . '/style.css', array(), $v );
+	
+	wp_enqueue_script( 'concealednation2024', get_template_directory_uri() . '/assets/main.js', array(), $v );
 }
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_style_sheet' );
+add_action( 'wp_enqueue_scripts', 'cn_enqueue_theme_assets' );
+
+/**
+ * Enqueue admin styles
+ */
+function cn_enqueue_admin_styles() {
+	wp_enqueue_style( 'cn-admin', get_template_directory_uri() . '/assets/admin.css', array(), wp_get_theme()->get( 'Version' ) );
+}
+add_action( 'admin_enqueue_scripts', 'cn_enqueue_admin_styles' );
 
 
 /**
  * Add Dashicons for use with block styles.
  */
-function enqueue_block_dashicons() {
+function cn_enqueue_block_dashicons() {
 	wp_enqueue_style( 'dashicons' );
 }
-add_action( 'enqueue_block_assets', __NAMESPACE__ . '\enqueue_block_dashicons' );
+add_action( 'enqueue_block_assets', 'cn_enqueue_block_dashicons' );
 
 
 /**
  * Add block style variations.
  */
-function register_block_styles() {
+function cn_register_block_styles() {
 
 	$block_styles = array(
 		'core/button'                    => array(
@@ -112,13 +130,13 @@ function register_block_styles() {
 		}
 	}
 }
-add_action( 'init', __NAMESPACE__ . '\register_block_styles' );
+add_action( 'init', 'cn_register_block_styles' );
 
 
 /**
  * Load custom block styles only when the block is used.
  */
-function enqueue_custom_block_styles() {
+function cn_enqueue_custom_block_styles() {
 
 	// Scan our styles folder to locate block styles.
 	$files = glob( get_template_directory() . '/assets/styles/*.css' );
@@ -139,13 +157,13 @@ function enqueue_custom_block_styles() {
 		);
 	}
 }
-add_action( 'init', __NAMESPACE__ . '\enqueue_custom_block_styles' );
+add_action( 'init', 'cn_enqueue_custom_block_styles' );
 
 
 /**
  * Register pattern categories.
  */
-function pattern_categories() {
+function cn_pattern_categories() {
 
 	$block_pattern_categories = array(
 		'concealednation2024/card'           => array(
@@ -178,25 +196,25 @@ function pattern_categories() {
 		register_block_pattern_category( $name, $properties );
 	}
 }
-add_action( 'init', __NAMESPACE__ . '\pattern_categories', 9 );
+add_action( 'init', 'cn_pattern_categories', 9 );
 
 
 /**
  * Remove last separator on blog/archive if no pagination exists.
  */
-function is_paginated() {
+function cn_is_paginated() {
 	global $wp_query;
 	if ( $wp_query->max_num_pages < 2 ) {
 		echo '<style>.blog .wp-block-post-template .wp-block-post:last-child .entry-content + .wp-block-separator, .archive .wp-block-post-template .wp-block-post:last-child .entry-content + .wp-block-separator, .blog .wp-block-post-template .wp-block-post:last-child .entry-content + .wp-block-separator, .search .wp-block-post-template .wp-block-post:last-child .wp-block-post-excerpt + .wp-block-separator { display: none; }</style>';
 	}
 }
-add_action( 'wp_head', __NAMESPACE__ . '\is_paginated' );
+add_action( 'wp_head', 'cn_is_paginated' );
 
 
 /**
  * Add a Sidebar template part area
  */
-function template_part_areas( array $areas ) {
+function cn_template_part_areas( array $areas ) {
 	$areas[] = array(
 		'area'        => 'sidebar',
 		'area_tag'    => 'section',
@@ -207,4 +225,102 @@ function template_part_areas( array $areas ) {
 
 	return $areas;
 }
-add_filter( 'default_wp_template_part_areas', __NAMESPACE__ . '\template_part_areas' );
+add_filter( 'default_wp_template_part_areas', 'cn_template_part_areas' );
+
+/**
+ * Replace the featured image with a video embed code if specified in the "Featured Video" ACF field group
+ *
+ * @param $value
+ * @param $post_id
+ * @param $display_field
+ * @param $custom_field_key
+ * @param $block
+ *
+ * @return mixed|string
+ */
+function cn_replace_featured_image_with_video( $value, $post_id, $display_field, $custom_field_key, $block ) {
+	if ( $display_field != 'featured_image' ) return $value;
+	
+	// Check if video embed is provided, replace the featured image if so
+	$embed_code = get_field( 'video_embed', $post_id );
+	if ( !$embed_code ) return $value;
+	
+	// oEmbed = True if the embed is a video URL and we should generate the embed code automatically
+	$use_oembed = get_field( 'video_oembed', $post_id );
+
+	if ( $use_oembed ) {
+		// Embed code was provided as a URL to an oEmbed service such as YouTube, convert to embed
+		$embed_code = wp_oembed_get( $embed_code );
+	}
+	
+	// Aspect ratio = Numeric aspect ratio such as 1.777 for 16:9
+	$aspect_ratio = get_field( 'video_aspect_ratio', $post_id );
+	
+	$class = 'cn-featured-video-embed';
+	$style = '';
+	
+	if ( $aspect_ratio ) {
+		$class .= ' has-aspect-ratio';
+		$style = 'style="aspect-ratio: ' . $aspect_ratio . ';"';
+	}
+	
+	return '<div class="'. esc_attr($class) .'" '. $style .'>' . $embed_code . '</div>';
+}
+add_filter( 'rs/post_field', 'cn_replace_featured_image_with_video', 10, 5 );
+
+/**
+ * Customize the post date block to show relative date, and to show post updated date if it is different from the published date
+ */
+function cn_customize_post_date_block( $block_content, $block ) {
+	// Only apply on single post pages
+	if ( ! is_singular('post') ) return $block_content;
+	
+	// Check if this is the post date block
+	if ( $block['blockName'] !== 'core/post-date' ) return $block_content;
+	
+	// Get the post ID
+	$post_id = get_the_ID();
+	
+	// Get the post date
+	$post_date = get_the_date( '', $post_id );
+	
+	// Get the post updated date
+	$post_updated_date = get_the_modified_date( '', $post_id );
+	
+	// Check if the post updated date is different than the post date
+	// Only applies on single post pages
+	if ( $post_date !== $post_updated_date ) {
+		// Show the updated date
+		$block_content = '<time class="published_date" datetime="' . esc_attr( get_the_date( 'c', $post_id ) ) . '">' . esc_html( $post_date ) . '</time> <time class="updated_date" datetime="' . esc_attr( get_the_modified_date( 'c', $post_id ) ) . '">Updated ' . esc_html( $post_updated_date ) . '</time>';
+	} else {
+		// Show the post date
+		$block_content = '<time class="published_date" datetime="' . esc_attr( get_the_date( 'c', $post_id ) ) . '">' . esc_html( $post_date ) . '</time>';
+	}
+	
+	$block_content = '<div class="cn_post_date">' . $block_content . '</div>';
+	
+	return $block_content;
+}
+add_filter( 'render_block', 'cn_customize_post_date_block', 10, 2 );
+
+function cn_customize_social_link_icons( $block_content, $block ) {
+	if ( 'core/social-link' != $block['blockName'] ) return $block_content;
+	
+	// Get the service to display
+	// facebook, instagram, rss (feed), x (twitter), youtube
+	$service = $block['attrs']['service'];
+	if ( $service == 'twitter' ) $service = 'x';
+	if ( $service == 'feed' ) $service = 'rss';
+	
+	$before        = explode( '<svg', $block_content );
+	$after         = explode( '</svg>', $before[1] );
+	
+	$icon_url = get_template_directory_uri() . '/assets/images/social-icons/' . $service . '.png';
+	$icon = '<img src="' . esc_attr($icon_url) . '" alt="' . esc_attr($service) . ' icon" />';
+	
+	$block_content = $before[0] . $icon . $after[1];
+	
+	return $block_content;
+}
+add_filter( 'render_block', 'cn_customize_social_link_icons', 10, 2 );
+
